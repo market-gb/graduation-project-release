e9d3bc253e1c5b02fd500f9b937fe587594466f7
AZor1973 <azor@mail.ru>
2022-07-11 13:01:24 +0300

    fix_integration_urls

--- core-service/src/main/java/ru/nhp/core/integrations/CartServiceIntegration.java
+++ core-service/src/main/java/ru/nhp/core/integrations/CartServiceIntegration.java
@@ -2,7 +2,6 @@ package ru.nhp.core.integrations;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
