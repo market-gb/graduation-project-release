56c79dc95e1d3e8a828b9cd635a294dcaa18a3ef
AndreyZorin <azor@mail.ru>
2022-07-19 11:05:16 +0300

    remove_front_service_from_eureka

--- front-service/src/main/java/ru/nhp/front/FrontSpringWebApplication.java
+++ front-service/src/main/java/ru/nhp/front/FrontSpringWebApplication.java
@@ -2,11 +2,9 @@ package ru.nhp.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
 public class FrontSpringWebApplication {
     public static void main(String[] args){
         SpringApplication.run(FrontSpringWebApplication.class, args);
