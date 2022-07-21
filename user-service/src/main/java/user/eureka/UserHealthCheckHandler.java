ed3e7d3904864a99c3f087e88cf990fde36d4674
AndreyZorin <azor@mail.ru>
2022-07-21 08:53:41 +0300

    объяснение для преподавателя

--- user-service/src/main/java/user/eureka/UserHealthCheckHandler.java
+++ user-service/src/main/java/user/eureka/UserHealthCheckHandler.java
@@ -5,6 +5,13 @@ import com.netflix.appinfo.InstanceInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
+/*
+С помощью классов UserHealthCheckHandler и UserHealthIndicator сервис сам может
+управлять своим статусом в eureka. Если в сервисе, например, упала БД,
+то для eureka он по-прежнему "в строю", а для других сервисов он уже не функционален.
+И в этом случае сервис сам меняет свой статус на "Down", а когда соединение с БД
+восстанавливается он сам меняет свой статус в eureka на "Up".
+*/
 
 @Component
 @RequiredArgsConstructor
