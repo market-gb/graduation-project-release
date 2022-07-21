67b9c7990952b3e24d951d4cec23cc002b10c9c7
Andrey <aedbaykal@gmail.com>
2022-07-21 12:20:35 +0300

    Починил запуск cart-service. Добавил redis в docker-compose.yaml

--- cart-service/src/main/java/ru/nhp/cart/configs/RedisConfig.java
+++ cart-service/src/main/java/ru/nhp/cart/configs/RedisConfig.java
@@ -6,7 +6,6 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
