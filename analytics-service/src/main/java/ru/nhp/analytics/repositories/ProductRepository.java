d6a5a68a55636da009be3ffe3451577ca394016f
EvgeniyYakushin <janeya@yandex.ru>
2022-07-29 08:16:10 +0300

    Создан контроллер аналитики товаров. Оба эндпойнта возвращают 404

--- analytics-service/src/main/java/ru/nhp/analytics/repositories/ProductRepository.java
+++ analytics-service/src/main/java/ru/nhp/analytics/repositories/ProductRepository.java
@@ -1,32 +1,31 @@
 package ru.nhp.analytics.repositories;
 
 import org.springframework.stereotype.Repository;
+import ru.nhp.api.dto.core.ProductAnaliticsDto;
 import ru.nhp.api.dto.core.ProductDto;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public class ProductRepository {
     private final HashMap<ProductDto, Long> products = new HashMap<>();
 
-    public void add (ProductDto product) {
+    public ProductDto add (ProductDto product) {
         if (products.get(product) == null) {
             products.put(product, 1L);
         } else {
             products.replace(product, products.get(product) + 1L);
         }
+        return product;
     }
 
-    public HashMap<String, Long> getProductsAnalitic () {
-        HashMap<String, Long> productsAnalitic = new HashMap<>();
-        products.forEach((key, val) -> productsAnalitic.put(key.getTitle(),val));
-        return productsAnalitic;
+    public List<ProductAnaliticsDto> getProductsAnalitic () {
+        List<ProductAnaliticsDto> productAnaliticsDtos = new ArrayList<>();
+        products.forEach((key, val) -> productAnaliticsDtos
+                .add(new ProductAnaliticsDto(key.getTitle(), val))
+        );
+        return productAnaliticsDtos;
     }
-
-    public Long getQuantityOneProduct(ProductDto product) {
-        return products.get(product);
-    }
-
-
-
 }
--- analytics-service/src/main/java/ru/nhp/analytics/services/ProductAnaliticService.java
+++ analytics-service/src/main/java/ru/nhp/analytics/services/ProductAnaliticService.java
@@ -3,20 +3,20 @@ package ru.nhp.analytics.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.nhp.analytics.repositories.ProductRepository;
+import ru.nhp.api.dto.core.ProductAnaliticsDto;
 import ru.nhp.api.dto.core.ProductDto;
-
-import java.util.HashMap;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductAnaliticService {
     private final ProductRepository productRepository;
 
-    public void addProduct (ProductDto product) {
-        productRepository.add(product);
+    public ProductDto addProduct (ProductDto product) {
+        return productRepository.add(product);
     }
 
-    public HashMap<String, Long> getProductAnalitic () {
+    public List<ProductAnaliticsDto> getProductAnalitic () {
         return productRepository.getProductsAnalitic();
     }
 
