b3b67296d3f283a600e82e3fdbc1c3e6802303e8
natsnegireva <natsnegireva@gmail.com>
2022-08-05 10:03:01 +0300

    Добавлен шаблон ЛК пользователя. Последние изменения в роутинге

--- front-service/src/main/resources/static/product/product.js
+++ front-service/src/main/resources/static/product/product.js
@@ -1,23 +1,19 @@
-angular.module('market-front').controller('productController', function ($scope, $routeParams, $rootScope, $http, $location, $localStorage) {
-    const contextPath = 'http://localhost:5555/core/';
+angular.module('market-front').controller('productController', function ($scope, $http, $location, $localStorage, $routeParams) {
 
-   $scope.productId = $routeParams.productId;
-
-   $scope.showProduct = function (productId) {
-         $http({
-             url: contextPath + 'api/v1/products' + productId,
-             method: 'GET'
-         }).then(function (response) {
-             $scope.Product = response.data;
-         });
-      };
-
+   $scope.loadProducts = function () {
+            $http({
+                url: 'http://localhost:5555/core/api/v1/products' + $routeParams.productId,
+                method: 'GET'
+            }).then(function (response) {
+                $scope.product = response.data;
+            });
+   };
 
     $scope.addToCart = function (productId) {
             $http.get('http://localhost:5555/cart/api/v1/carts/' + $localStorage.springWebGuestCartId + '/add/' + productId)
                 .then(function (response) {
-                });
+            });
     }
 
-    $scope.showProduct();
+    $scope.loadProducts();
 });
