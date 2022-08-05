9d660493bcd64a372c35657f1f6d3cd010bc7dca
natsnegireva <natsnegireva@gmail.com>
2022-08-05 09:54:34 +0300

    Создана страница товара

--- front-service/src/main/resources/static/store/store.js
+++ front-service/src/main/resources/static/store/store.js
@@ -12,7 +12,8 @@ angular.module('market-front').controller('storeController', function ($scope, $
                 title_part: $scope.filter ? $scope.filter.title_part : null,
                 min_price: $scope.filter ? $scope.filter.min_price : null,
                 max_price: $scope.filter ? $scope.filter.max_price : null,
-                category_title: $scope.filter ? $scope.filter.category_title : null
+                category_title: $scope.filter ? $scope.filter.category_title : null,
+                category_id: $scope.filter ? $scope.filter.category_id : null
             }
         }).then(function (response) {
             $scope.ProductsPage = response.data;
--- front-service/src/main/resources/static/welcome/welcome.js
+++ front-service/src/main/resources/static/welcome/welcome.js
@@ -1,20 +1,19 @@
 angular.module('market-front').controller('welcomeController', function ($scope, $rootScope, $http, $location, $localStorage, $routeParams) {
     const contextPath = 'http://localhost:5555/core/';
-    $rootScope.categoryId = null;
+     $rootScope.categoryId = null;
 
-    $scope.loadCategory = function (pageIndex = 1) {
-        $http({
-            url: contextPath + 'api/v1/categories',
-            method: 'GET'
-        }).then(function (response) {
-            $scope.Category = response.data;
-        });
-    };
+     $scope.loadCategory = function (pageIndex = 1) {
+            $http({
+                url: contextPath + 'api/v1/categories',
+                method: 'GET'
+            }).then(function (response) {
+                $scope.Category = response.data;
+            });
+        };
 
-    $rootScope.showProductsByCategory = function (categoryTitle) {
-       $location.path('/store/') + categoryTitle;
-    };
-
+     $rootScope.showProductsByCategory = function (categoryId) {
+        $location.path('/store/' +  categoryId);
+     };
 
     $scope.loadCategory();
 });
