0827b01586fc09c916906fe1c3cac553d1a85a82
Ivan <ivan_semenov@bk.ru>
2022-08-11 23:24:28 +0300

    GB-109 созданы тесты для UserReposirory. Убрал закомментированный код.

--- user-service/src/test/java/ru/nhp/user/UserRepositoryTest.java
+++ user-service/src/test/java/ru/nhp/user/UserRepositoryTest.java
@@ -6,28 +6,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.ActiveProfiles;
 import ru.nhp.user.repositories.UserRepository;
 import ru.nhp.user.entites.Role;
 import ru.nhp.user.entites.User;
 
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
 @DataJpaTest
-//@ActiveProfiles("test")
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private TestEntityManager entityManager;
-//    private static final String USERNAME = "test_username";
-//    private static final String PASSWORD = "test_password";
-//    private static final String EMAIL = "test_email";
 
-//    private Role firstRole;
     private User user;
     private LocalDateTime time;
 
@@ -47,7 +40,7 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void changeUpdateAtTest(){
+    public void changeUpdateAtTest() {
         userRepository.changeUpdateAt(1L);
         User user = entityManager.find(User.class, 1L);
         Assertions.assertNotEquals(time, user.getUpdatedAt());
